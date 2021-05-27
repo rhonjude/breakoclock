028e46d9c371e5a7a60cd4c0e0d22318fcbf19cd
rhonjude <rhonjude007@gmail.com>
2021-05-27 23:52:06 +0530

    message

--- database/migrations/2021_05_06_170755_create_products_table.php
+++ database/migrations/2021_05_06_170755_create_products_table.php
@@ -14,7 +14,7 @@ class CreateProductsTable extends Migration
     public function up()
     {
         Schema::create('products', function (Blueprint $table) {
-            $table->increment('pid');
+            $table->increments('pid');
             $table->string('product_name');
             $table->integer('cid');
             $table->string('description');
