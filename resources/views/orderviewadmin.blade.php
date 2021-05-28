33ba67fa63a279e0072ff9ce2b0a4b291a80cc4d
rhonjude <rhonjude007@gmail.com>
2021-05-28 11:38:35 +0530

    message

--- resources/views/orderviewadmin.blade.php
+++ resources/views/orderviewadmin.blade.php
@@ -23,7 +23,7 @@
         }
         .container{
     
-    background: url("https://images.unsplash.com/photo-1501339847302-ac426a4a7cbb?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1057&q=80");
+    background: url("https://images.unsplash.com/photo-1514933651103-005eec06c04b?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=967&q=80");
     background-repeat: no-repeat;
     width:100%;
     height:100vh;         
